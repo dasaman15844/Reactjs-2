7b6ebfbf0be9359c0fb0907b744f139039abc1e3
dasaman15844 <dasaman15844@gmail.com>
2022-04-04 21:17:01 +0530

    install

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+    <form>
+    <div className='col-75'>
+      Enter your name: <div>
+        <input type="text" name='name'/>
+        </div>
+        </div><div className='col-75'>
+        
+      Enter your password:<div>
+        <input type="text" name='password'/>
+        </div>
+        </div>
+
+        <div className='col-75'>
+        
+      Address:<div>
+        <input type="text" name='password'/>
+        </div>
+        </div>
+        
+        <div className='row'>
+    <input type="submit" value="Submit"  />
+  </div>
+    </form>
     </div>
   );
 }
